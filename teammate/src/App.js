5feb042173fa7a86819b3b3f549658eb03454b3e
Chen Xiaotong <e0492455@u.nus.edu>
2022-02-16 17:12:49 +0800

    Initial commit

---
+++ teammate/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import PageProfile from './PageProfile';
+
+function App() {
+  return (
+    <div>
+      <PageProfile />
+    </div>
+  );
+}
+
+export default App;
